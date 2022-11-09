ab755ca23d68862c990955c379be6c767f5e5df2
mihai-dulgheru <63505058+mihai-dulgheru@users.noreply.github.com>
2022-11-09 17:23:28 +0200

    modify process-score-sheet.js

--- controllers/scoreSheet/recognize.js
+++ controllers/scoreSheet/recognize.js
@@ -1,9 +1,39 @@
+const OEM = require('tesseract.js/src/constants/OEM.js');
+const PSM = require('tesseract.js/src/constants/PSM.js');
+const { createWorker } = require('tesseract.js');
 const { processScoreSheet } = require('../../functions');
+
+const worker = createWorker({});
 
 module.exports = async (req, res) => {
   const { image } = req.files;
 
-  const processed = await processScoreSheet(image.data);
+  const results = await processScoreSheet(image.data);
+  const sheet = [];
 
-  return res.status(200).send(processed);
+  await worker.load();
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+  await worker.setParameters({
+    tessedit_ocr_engine_mode: OEM.TESSERACT_LSTM_COMBINED,
+    tessedit_pageseg_mode: PSM.SINGLE_LINE,
+  });
+
+  for (let i = 0; i < 50; i++) {
+    const {
+      data: { text: white },
+    } = await worker.recognize(results[0][i]);
+    const {
+      data: { text: black },
+    } = await worker.recognize(results[1][i]);
+    sheet.push({
+      line: i + 1,
+      white: white.split('\n')[0].trim().split(' ')[0] || '',
+      black: black.split('\n')[0].trim().split(' ')[0] || '',
+    });
+  }
+
+  await worker.terminate();
+
+  return res.status(200).send(sheet);
 };
--- functions/process-score-sheet.js
+++ functions/process-score-sheet.js
@@ -1,22 +1,64 @@
 const Jimp = require('jimp');
-// const { kernel } = require('../data/kernel');
+const { kernel } = require('../data/kernel');
+
+const f = 4;
+const mode = Jimp.RESIZE_BEZIER;
+const mime = Jimp.MIME_BMP;
+
+const getHeight = (i, h, p) => {
+  if (i === 24) return h;
+  return h + p;
+};
 
 const processScoreSheet = async (img) => {
   const image = await Jimp.read(img);
-  // const { width, height } = image.bitmap;
 
-  image
-    // .crop(
-    //   parseInt(width * 0.3),
-    //   parseInt(height * 0.19),
-    //   parseInt(width * 0.3),
-    //   parseInt(height * 0.21)
-    // )
-    // .convolute(kernel)
-    // .contrast(0.3)
-    .write('output/processed.jpeg');
+  image.convolute(kernel).contrast(0.2);
 
-  return await image.getBufferAsync(Jimp.MIME_JPEG);
+  const column1 = image.clone();
+  const column2 = image.clone();
+  const { width, height } = image.bitmap;
+
+  column1.crop(width * 0.075, height * 0.2295, width * 0.425, height * 0.685);
+  // .write('output/column-1.jpeg');
+
+  column2.crop(width * 0.5375, height * 0.2295, width * 0.425, height * 0.685);
+  // .write('output/column-2.jpeg');
+
+  const results = new Array(2).fill(null).map(() => new Array(50).fill(null));
+
+  const w = column1.bitmap.width / 2;
+  const h = column1.bitmap.height / 25;
+  const p = h * 0.2;
+  for (let i = 0; i < 25; i++) {
+    const column1_white = column1.clone();
+    const y = h * i;
+
+    column1_white.crop(0, y, w, getHeight(i, h, p)).scale(f, mode);
+    // .write(`output/column-1-white-${i + 1}.jpeg`);
+    results[0][i] = await column1_white.getBufferAsync(mime);
+
+    const column1_black = column1.clone();
+    column1_black.crop(w, y, w, getHeight(i, h, p)).scale(f, mode);
+    // .write(`output/column-1-black-${i + 1}.jpeg`);
+    results[1][i] = await column1_black.getBufferAsync(mime);
+  }
+
+  for (let i = 0; i < 25; i++) {
+    const column2_white = column2.clone();
+    const y = h * i;
+
+    column2_white.crop(0, y, w, getHeight(i, h, p)).scale(f, mode);
+    // .write(`output/column-2-white-${i + 1}.jpeg`);
+    results[0][i + 25] = await column2_white.getBufferAsync(mime);
+
+    const column2_black = column2.clone();
+    column2_black.crop(w, y, w, getHeight(i, h, p)).scale(f, mode);
+    // .write(`output/column-2-black-${i + 1}.jpeg`);
+    results[1][i + 25] = await column2_black.getBufferAsync(mime);
+  }
+
+  return results;
 };
 
 module.exports = processScoreSheet;
